3939609822f3191c591afdff52e8c547aa7490fc
Yaroslav Grabovych <ygrabovych@gmail.com>
2024-03-03 20:06:02 +0200

    clean

--- src/routes/storesRouter.js
+++ src/routes/storesRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const {getAllProducts} = require("../controllers/storesController");
 
-// Обробник для GET-запиту на маршрут /store
 router.get('/pharm/store', getAllProducts);
 
 module.exports = router;
